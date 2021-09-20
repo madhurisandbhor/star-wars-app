851be58e65bde8daf6404755b97deedbee8784e1
Madhuri Sandbhor <madhuri.s@ignitiv.com>
2021-09-20 10:07:16 +0200

    minor change

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "App";
+import App from "app";
 import "./mocks/intersectionObserverMock";
 
 test("renders app component", () => {
